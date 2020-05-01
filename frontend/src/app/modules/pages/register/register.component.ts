b681e28dbfe87c70a605eb3c3bec9309a1866b8f
Guilherme Stephaneli <guilhermesthephaneli507@gmail.com>
2020-05-01 01:12:08 -0300

    Iniciando tela de rehistro

--- frontend/src/app/modules/pages/register/register.component.ts
+++ frontend/src/app/modules/pages/register/register.component.ts
@@ -1,4 +1,7 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroupDirective, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+              private snackBar: MatSnackBar,
+              private router: Router) { }
 
   ngOnInit() {
+    this.create();
+  }
+
+  openSnackBar(message) {
+    this.snackBar.open(message, null, { duration: 3000 });
+  }
+
+  create(): void {
+    this.form = this.formBuilder.group({
+
+      name: [null, [Validators.required, Validators.maxLength(150)]],
+      email: [null, [Validators.required, Validators.maxLength(150)]],
+      confirmEmail: [null, [Validators.required, Validators.maxLength(150)]],
+      password: [null, [Validators.required,
+                        Validators.maxLength(20),
+                        Validators.minLength(8),
+                        Validators.pattern(
+                          /^(?=.*[A-Z])(?=.*['!\{@\}#\$%\^&\*,+./\_()\[¨\]=\-])(?=.*[0-9])(?=.*[a-z]).*$/)
+                        ]
+                      ],
+      confirmPassword: [null, [Validators.required,
+                        Validators.maxLength(20),
+                        Validators.minLength(8),
+                        Validators.pattern(
+                          /^(?=.*[A-Z])(?=.*['!\{@\}#\$%\^&\*,+./\_()\[¨\]=\-])(?=.*[0-9])(?=.*[a-z]).*$/)
+                        ]
+                      ],
+
+    });
+  }
+
+  onSubmit(formDirective: FormGroupDirective): void {
+
+    if (this.form.valid) {
+
+    console.log(JSON.stringify(this.form.value));
+
+    } else {
+    this.openSnackBar('Informe todos os campos obrigatórios');
+    }
+
   }
 
 }
--- frontend/src/app/modules/pages/register/register.module.ts
+++ frontend/src/app/modules/pages/register/register.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register.component';
 import { RegisterRoutingModule } from './register-routing.module';
+import { MatCardModule, MatFormFieldModule } from '@angular/material';
 
 @NgModule({
   imports: [
     SharedModule,
     CommonModule,
-    RegisterRoutingModule
+    RegisterRoutingModule,
+    MatCardModule,
+    MatFormFieldModule
   ],
 
   declarations: [RegisterComponent],
